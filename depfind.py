ca9d9ab022686d455c05d44186269024f5076074
metehan <mthnzbk@gmail.com>
2016-05-22 11:33:13 +0300

    olmayan veriler de listeleniyor.

--- depfind.py
+++ depfind.py
@@ -1,3 +1,6 @@
+#-*- coding: utf-8 -*-
+#!/usr/bin/env python
+
 from pisi.db.filesdb import FilesDB
 import sys
 import re
@@ -11,20 +14,41 @@ package_list = []
 # find_package(Qt5 $
 
 with open(cmake_file) as file_cmake:
+	# dosya satır satır okunuyor ve istenilen veriler ayıklanıyor.
 	for line in file_cmake.readlines():
 		compile = re.compile(r"^find_package\((.*) \$")
 		obje = compile.search(line)
 		if obje:
+			# istenilen veri varsa listeye ekliyoruz.
 			package_list.append(obje.groups()[0])
 
-
-print package_list
 files_db = FilesDB()
 
+print package_list # Aranacak verileri yazdırıyoruz.
+
+counter = 0
 
 for package in package_list:
 	liste = files_db.search_file(package)
-	for l in liste:
-		for li in l[-1]:
-			if li.endswith("cmake"):
-				print package, "-", l[0], "-", li
+
+	#aranan veri değer döndürürse döngü başlar
+	if liste:
+		# liste içinde liste.
+		for li in liste:
+			# ilk liste ögesi(li[0]) paket adı, ikincisi de(li[-1]) liste halinde aranan kelimenin geçtiği dosyalar.
+			for l in li[-1]:
+				# dosyaların uzantısı *.cmake olanları ayıklayıp çıktı veriyoruz.
+				if l.endswith("cmake"):
+					print package, "-", li[0], "-", l
+					counter += 1 # sayaç ile cmake dosyalarının varlığı tespit ediliyor.
+		# sayaç artmamışsa demekki cmake dosyası yoktur ve bunun bilgisi çıktıya yansır.
+		if not counter:
+			print package, "-", u"Bulunamadı"
+		# sayaç artmışsa sıfırlanır ki bir sonraki döngü de hata oluşmasın.
+		else:
+			counter = 0
+
+	# liste boş ise aranan veriye sahip paket kurulu değildir.
+	else:
+		print package, "-", u"Bulunamadı"
+			
